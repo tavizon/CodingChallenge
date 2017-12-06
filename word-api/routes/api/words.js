cfbf8c9b679bb0da133836b9a1637d526d493acd
Jesse Tavizon <tavizon@kink.com>
2017-12-05 17:19:52 -0800

    words api route with errors and validation

--- word-api/routes/api/words.js
+++ word-api/routes/api/words.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const validUrl = require('valid-url');
+
 module.exports = function (router) {
 	/**
 	 * Count words on a webpage
@@ -10,7 +12,13 @@ module.exports = function (router) {
 	 * @return {*} - JSON with the word count result.
 	 */
 	function words(request, response) {
-		return response.send('Hello World!');
+		response.setHeader('Content-Type', 'application/json');
+		if (validUrl.isWebUri(request.query.url)) {
+
+			return response.send(request.query);
+		}
+
+		return response.status(500).send({error: 'URL was invalid or not supplied'});
 	}
 
 	router.get('/api/words', words);
